3269009b30d4c1844898c12975ad99f86b3e7251
rybaier <rybaier@gmail.com>
2022-04-28 15:46:06 -0700

    hackerrank challenges for job interview. stuck on blanaced brackets. going to try using a stack datat stucture, as this seems to be the best way

--- js playground/index.js
+++ js playground/index.js
@@ -22,4 +22,57 @@ function camelCase (string) {
     
     console.log(word3)
 }
-camelCase('the-works-three')
+// camelCase('the-works-three')
+
+// let principal = this.targets.find("principal").value
+// let interest = this.targets.find("interest").value
+// let years = this.targets.find("years").value
+// // let amount = principal * ((1 + (interest/100)) ** years)
+// let annual = this.targets.find("annual").value
+
+// this.targets.find("amount").innerHTML = "The amount you will have is: $" + amount 
+
+let principal = 100
+let interest = 2
+let years = 10
+let amount = 0
+let annual = 5
+// console.log(principal, years, interest, annual)
+// let newPrincipal = 0
+//  for (i = 0; i < years; i++){
+//     newPrincipal = annual + (principal * (1 + (interest/100)))
+//     principal = newPrincipal
+//     amount = principal
+//     console.log(newPrincipal, principal)        
+// }
+    
+
+// console.log(amount, 'works')
+
+
+function fizzBuzz(n) {
+    // Write your code here
+  
+        for (let i = 1; i<=n; i++) {
+            if( i % 3 === 0 && i % 5 === 0) { console.log('FizzBuzz')} 
+            else if ( i % 3 === 0 ) { console.log('Fizz') } 
+            else if ( i % 5 === 0)  console.log('Buzz')   
+            else  console.log(i)
+            
+            }
+    }
+     
+// fizzBuzz(15)
+
+function matchingBraces(braces) {
+    // Write your code here
+    const matchSquare = '[]'
+    const matchCurly = '{}'
+    const matchParenth = '()'
+    console.log(braces)
+    let newArray = braces.toString().split(',')
+    console.log(newArray)
+    
+}
+
+matchingBraces(['{}[]()', '(){]}'])
